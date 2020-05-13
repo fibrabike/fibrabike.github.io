35fa1d07ac4c5db5358adba784389902e97ef520
Gianvito Taneburgo <gianvito.taneburgo@brightcomputing.com>
2020-05-13 20:52:31 +0200

    * ESC event * URL modal fix

---
+++ js/fibra.js
@@ -0,0 +1,15 @@
+document.onkeydown = function(evt) {
+    let modal;
+    let tokens;
+    if (evt.key === "Escape") {
+        // console.log("ESC DETECTED")
+        tokens = window.location.href.split("#")
+        modal = tokens[tokens.length - 1]
+        if (modal.startsWith("portfolioModal")) {
+            // console.log(modal)
+            modal = document.getElementById("close-button-".concat(modal))
+            modal.click()
+            window.history.pushState("", "", '/');
+        }
+    }
+};
