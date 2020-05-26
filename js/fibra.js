29da30e4849591efcf1e596a846f03465b588fcc
Gianvito Taneburgo <gianvito.taneburgo@brightcomputing.com>
2020-05-26 19:31:36 +0200

    Fancy modal urls

--- js/fibra.js
+++ js/fibra.js
@@ -1,18 +1,3 @@
-// document.onkeydown = function(evt) {
-//     let modal;
-//     let tokens;
-//     if (evt.key === "Escape") {
-//         // console.log("ESC DETECTED")
-//         tokens = window.location.href.split("#")
-//         modal = tokens[tokens.length - 1]
-//         if (modal.startsWith("portfolioModal")) {
-//             // console.log(modal)
-//             document.getElementById("close-button-".concat(modal)).click();
-//             history.back();
-//         }
-//     }
-// };
-
 function prefillEmailForm(modal, bike_title) {
     document.getElementById("close-button-".concat(modal)).click();
     document.getElementById("message").innerText = "Ciao, ti scrivo a proposito di " + bike_title + ". ";
