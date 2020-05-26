89688144212fa4e12046922ace800dd322ca4a52
Gianvito Taneburgo <gianvito.taneburgo@brightcomputing.com>
2020-05-26 15:52:37 +0200

    Custom JS for email prefill

--- js/fibra.js
+++ js/fibra.js
@@ -12,3 +12,9 @@
 //         }
 //     }
 // };
+
+function prefillEmailForm(modal, bike_title) {
+    document.getElementById("close-button-".concat(modal)).click();
+    document.getElementById("message").innerText = "Ciao, ti scrivo a proposito di " + bike_title + ". ";
+    document.getElementById("mail_subject").value = bike_title;
+}
